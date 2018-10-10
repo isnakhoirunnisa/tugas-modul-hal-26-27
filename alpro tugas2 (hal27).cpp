d94eee7b8796b89e8607f4627c0f2efd44997605
isnakhoirunnisa <43692394+isnakhoirunnisa@users.noreply.github.com>
2018-10-10 23:26:42 +0700

    Add files via upload

---
+++ alpro tugas2 (hal27).cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+main (void){
+	int angka;
+	cout<<"Masukkan angka : ";
+	cin>>angka;
+	cout<<endl;
+	
+(angka > 36) ? cout<<"Suhu Panas" : cout<<"Suhu Dingin";
+		}
+
---
+++ alpro tugas2(hal26).cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+main(void)
+{
+int bilangan;
+cout<<"masukan bilangan:";
+cin>>bilangan;
+(bilangan%2==0) ? cout<<"Yang Anda Masukkan Adalah Bilangan Genap" : cout<<"Yang Anda Masukkan Adalah Bilangan Ganjil";
+getch();
+}
+
